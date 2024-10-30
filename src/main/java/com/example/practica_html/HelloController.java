dcebeb1558ee867624f3b05898ef27744b2fe6d2
Алексей Зенков <zenkov.m700@mail.ru>
2024-10-30 21:46:20 +0300

    Добавлен редактор html

--- src/main/java/com/example/practica_html/HelloController.java
+++ src/main/java/com/example/practica_html/HelloController.java
@@ -1,14 +1,47 @@
 package com.example.practica_html;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.web.HTMLEditor;
+import javafx.scene.web.WebView;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private HTMLEditor htmlEditor;
+
+    @FXML
+    private WebView webView;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private Button htmlToText;
+
+    @FXML
+    private Button textToHtml;
+
+    @FXML
+    private void initialize() {
+        // Обработчик для кнопки "Преобразовать в HTML"
+        htmlToText.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent arg0) {
+                htmlEditor.setHtmlText(textArea.getText());
+            }
+        });
+
+        // Обработчик для кнопки "Преобразовать в текст"
+        textToHtml.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent arg0) {
+                textArea.setText(htmlEditor.getHtmlText());
+            }
+        });
     }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.practica_html {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
 
 
     opens com.example.practica_html to javafx.fxml;
